1a8e05eb54c9864b2a18aa2442e4bd54f2c76d2e
kaemic3 <hazedzephyr@gmail.com>
2024-09-29 13:44:02 -0700

    Fix link to GMu demo page.

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="flex items-center justify-center">
         <ul className=" w-full md:w-3/6 text-xl space-y-3 pb-3 TextColor">
             <Card asChild className="p-8">
-                <a href="/GMu">
+                <a href="/Demos/GMu">
                     <Grid columns="2">
                         <Heading size="7" className="">GMu</Heading>
                         <Text className="text-right py-1 pr-1">Learn more about my GameBoy emulator.</Text>
